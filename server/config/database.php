69110963775d0ff63fd6a0ba32698b14c1249f9e
Martynas Treinys <martynas@satalia.com>
2021-03-17 12:19:04 +0200

    Finish setting up project with MongoDB fix

--- server/config/database.php
+++ server/config/database.php
@@ -90,7 +90,21 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => 'mongodb://user@db:27017',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'homestead'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
+//            'options' => [
+//                // here you can pass more settings to the Mongo Driver Manager
+//                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
+//
+//                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+//            ],
+        ],
     ],
 
     /*
