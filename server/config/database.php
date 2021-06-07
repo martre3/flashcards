9531c6fa205dd909e6ed5e1e2e7e1d608d454588
Martynas Treinys <martynas7928@gmail.com>
2021-06-08 01:10:01 +0300

    commit db pass :(

--- server/config/database.php
+++ server/config/database.php
@@ -98,7 +98,7 @@ return [
 //            'port' => env('DB_PORT', 27017),
 //            'database' => env('DB_DATABASE', 'homestead'),
 //            'username' => env('DB_USERNAME', 'homestead'),
-//            'password' => env('DB_PASSWORD', 'secret'),
+            'password' => 'flashpass',
             'options' => [
 //                // here you can pass more settings to the Mongo Driver Manager
 //                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
