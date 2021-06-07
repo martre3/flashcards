4f5b3ae7ae1f81ac09b08d7ebadb087c87a5421a
Martynas Treinys <martynas7928@gmail.com>
2021-06-08 01:02:20 +0300

    Test

--- server/config/database.php
+++ server/config/database.php
@@ -94,17 +94,17 @@ return [
             'driver' => 'mongodb',
             'dsn' => 'mongodb+srv://flashcards@cluster0.qil8g.mongodb.net/flashcards?retryWrites=true&w=majority',
 //            'dsn' => 'mongodb://user@db:27017',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'homestead'),
-            'username' => env('DB_USERNAME', 'homestead'),
-            'password' => env('DB_PASSWORD', 'secret'),
-//            'options' => [
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'port' => env('DB_PORT', 27017),
+//            'database' => env('DB_DATABASE', 'homestead'),
+//            'username' => env('DB_USERNAME', 'homestead'),
+//            'password' => env('DB_PASSWORD', 'secret'),
+            'options' => [
 //                // here you can pass more settings to the Mongo Driver Manager
 //                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
 //
-//                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-//            ],
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+            ],
         ],
     ],
 
